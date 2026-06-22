fc46c30b657fc0b4f47cc27e974e8d06519d29ce
bhavana-venugopal <bhavana@gritworks.ai>
2026-06-22 10:29:55 +0530

    handle back

--- src/pages/ShikshalokamVoiceChat/dynamic-voice-chat.js
+++ src/pages/ShikshalokamVoiceChat/dynamic-voice-chat.js
@@ -1099,6 +1099,10 @@ const DynamicVoiceChat = ({
     }
   }, [])
 
+  function stayOnPage() {
+    window.history.pushState(null, "", window.location.href)
+  }
+
   /**
    * Browser back button handling - intercepts browser navigation
    * Shows guest popup for special flows or navigates to previous page
@@ -1112,7 +1116,7 @@ const DynamicVoiceChat = ({
         if (ssoNavigationTriggered && accessToken) {
           navigate(-2)
         } else {
-          navigateBack()
+          showGuestPopup(navigateBack, stayOnPage)
         }
       } else {
         setLanguage(languageList[0].value)
