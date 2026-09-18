1e53fc7de836e3504f0fe0ae94fe610b52c0d4cb
farhanp1502 <farhanpasha1502@gmail.com>
2026-09-18 11:56:43 +0530

    resolved the comments

--- src/pages/ShikshalokamVoiceChat/dynamic-voice-chat.js
+++ src/pages/ShikshalokamVoiceChat/dynamic-voice-chat.js
@@ -1529,9 +1529,11 @@ const DynamicVoiceChat = ({
     }
 
     vv.addEventListener('resize', update)
+    vv.addEventListener('scroll', update)
 
     return () => {
       vv.removeEventListener('resize', update)
+      vv.removeEventListener('scroll', update)
       document.documentElement.style.removeProperty('--vkb-offset')
     }
   }, [])
